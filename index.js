4c054eb9c49fb4dc508a2ecab4fead26564dbe81
ed <edward@commodum.io>
2023-10-12 13:05:54 +0000

    Added peer discovery

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const EventEmitter = require('node:events');
+const net = require("node:net");
 
 const initialIps = [ "165.232.126.207", "167.71.44.7", "164.92.138.136", "185.246.86.71", "134.122.64.6", "39.59.237.19" ];
 const inProgress = [];
-const successful = []
+const successful = [];
 const failed = [];
+const successfulPeers = [];
+const failedPeers = [];
 
 const checker = new EventEmitter();
 checker.numRunning = 0;
+checker.numPeersRunning = 0;
 checker.numChecked = 0;
 checker.numFailures = 0;
 checker.numSuccesses = 0;
@@ -17,17 +21,25 @@ const main = () => {
         console.log("Awaiting responses...")
     }, 3000);
 
-    checker.on('newIp', checkIp);
+    checker.on("newIp", checkIp);
+    checker.on("newPeer", checkPeer);
     checker.on("done", () => {
         clearInterval(intervalId);
         console.log("Done.");
         console.log(`${checker.numChecked} IPs checked.`);
-        console.log(`${checker.numFailures} failures.`);
-        console.log(`${checker.numSuccesses} successes.`);
+        console.log(`${checker.numFailures} RPC failures.`);
+        console.log(`${checker.numSuccesses} RPC successes.`);
+        console.log(`${failedPeers.length} peer failures.`);
+        console.log(`${successfulPeers.length} peer successes.`);
 
-        console.log("Available RPCs detected at the following addresses: ");
+        console.log("\nAvailable RPCs detected at the following addresses: ");
         for (let ip of successful) {
             console.log(`http://${ip}:26657`);
+        }
+
+        console.log("\nAvailable peers detected at the following addresses: ");
+        for (let peer of successfulPeers) {
+            console.log(`${peer}`);
         }
     });
 
@@ -41,7 +53,7 @@ const checkIp = async (ip) => {
 
     checker.numRunning++;
 
-    console.log(`Checking for available TM RPC at ip: ${ip}`);
+    // console.log(`Checking for available TM RPC at ip: ${ip}`);
     const rpcAddr = `http://${ip}:26657/net_info`
     inProgress.push(ip);
 
@@ -51,7 +63,7 @@ const checkIp = async (ip) => {
         checker.numRunning--;
         checker.numChecked++;
         checker.numFailures++;
-        if (checker.numRunning == 0) {
+        if (checker.numRunning == 0 && checker.numPeersRunning == 0) {
             checker.emit("done");
         }
     });
@@ -68,14 +80,49 @@ const checkIp = async (ip) => {
             continue
         }
         checker.emit("newIp", peer.remote_ip);
+        checker.emit("newPeer", peer);
     }
 
     checker.numRunning--;
     checker.numChecked++;
     checker.numSuccesses++;
-    if (checker.numRunning == 0) {
+    if (checker.numRunning == 0 && checker.numPeersRunning == 0) {
         checker.emit("done");
     }
+
+}
+
+const checkPeer = (peer) => {
+    
+    checker.numPeersRunning++
+
+    const sock = new net.Socket();
+
+    setTimeout(() => {
+        sock.destroy("destroying socket")
+        if (checker.numRunning == 0 && checker.numPeersRunning == 0) {
+            checker.emit("done");
+        }
+    }, 5000);
+
+    sock.connect(port="26656", host=`${peer.remote_ip}`, () => {
+        // console.log(`Connection successful to ${peer.remote_ip}:26656`);
+        successfulPeers.push(`${peer.node_info.id}@${peer.remote_ip}:26656`);
+        checker.numPeersRunning--
+        sock.destroy()
+        if (checker.numRunning == 0 && checker.numPeersRunning == 0) {
+            checker.emit("done");
+        }
+    });
+    
+    sock.on('error', (err) => {
+        // console.log("Failed to connect: ", err)
+        failedPeers.push(`${peer.node_info.id}@${peer.remote_ip}:26656`);
+        checker.numPeersRunning--
+        if (checker.numRunning == 0 && checker.numPeersRunning == 0) {
+            checker.emit("done");
+        }
+    });
 
 }
 
